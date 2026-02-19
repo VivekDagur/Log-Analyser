a53073b0c379c8f91486ce991f6bb05eb8722047
Vivek Dagur <vivekdagur@Viveks-MacBook-Air.local>
2026-02-19 15:23:17 +0530

    added system stability check using amount of errors are there in log

--- log-analyser.py
+++ log-analyser.py
@@ -1,16 +1,31 @@
 with open('sample.log','r') as file:
-  error=0
-  info=0
-  warning=0
-  for line in file:
-     if 'ERROR' in line:
-        error+=1
-     elif 'INFO' in line:
-        info+=1
-     elif 'WARNING' in line:
-        warning+=1
-  print("Error: ",error)      
-  print("Info: ", info)      
-  print("Warning: ",warning)      
+   error=0
+   info=0
+   warning=0
+   for line in file:
+      if 'ERROR' in line:
+         error+=1
+      if 'INFO' in line:
+         info+=1
+      if 'WARNING' in line:
+         warning+=1
+   total=error+info+warning
+   if total==0:
+       print("SYSTEM EMPTY")
+   elif total!=0:
+      rate=int((error/total)*100)
+      if rate==0:
+            print("SYSTEM STABLE")
+      elif rate<=20:
+         print("SYSTEM DEGRADED")
+      else:
+          print("SYSTEM CRITICAL")    
+
+
+
         
       
+
+
+
+
